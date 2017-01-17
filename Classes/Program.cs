e787862673d4a6b352d23efac8c6902dd68a231f
Cameron Reid <cam12win@gmail.com>
2017-01-17 10:38:26 +0000

    Code to add user is in. Can't think of any loopholes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,75 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            //Defining the variables
+            string name;
+            int age;
+            string email;
+
+            Console.WriteLine("Hello");
+
+            while ("pigs" != "flying")
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+                if (name == "")
+                {
+                    Console.WriteLine("Please enter your name");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            
+            
+            
+            //Loop to make sure age is a number
+            while ("pigs" != "flying")
+            {
+                Console.WriteLine("Thank you {0}, what is your age? (As a number please)", name);
+                string stringAge = Console.ReadLine();
+                //Leaves loop if age is a number
+                if (int.TryParse(stringAge, out age))
+                {
+                    break;
+                }
+                //Continues if it isn't.
+                else
+                {
+                    Console.WriteLine("Please enter a number");
+                    continue;
+                }
+            }
+
+            while ("pigs" != "flying")
+            {
+                Console.WriteLine("What is your email? (If none, leave blank)");
+                email = Console.ReadLine();
+                if (email == "")
+                {
+                    //Email is allowed to be blank
+                    break;
+                }
+                if (email.Contains("@") && email != "")
+                {
+                    //Leaves loop if email contains @
+                    break;
+                }
+                else
+                {
+                    //Email must contain an @ symbol
+                    Console.WriteLine("Please enter a valid email address or nothing");
+                    continue;
+                }
+            }
+
+            Person personA = new Person(name, age, email);
+            Console.WriteLine("Thank you {0}", personA.ToString());
+
+            
         }
     }
 }
