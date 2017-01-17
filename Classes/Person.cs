d00472e1bf00d9d527d5fd87b9706fd93edcf33e
Cameron Reid <cam12win@gmail.com>
2017-01-17 10:37:54 +0000

    Added person class and initialisers

---
+++ Classes/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public string email;
+
+        public Person(string name, int age, string email)
+        {
+            this.name = name;
+            this.age = age;
+            if (email == "")
+            {
+                this.email = "nil";
+            }
+            else
+            {
+                this.email = email;
+            }
+            
+        }
+
+        /// <summary>
+        /// Return string of information rather than breaking
+        /// </summary>
+        public override string ToString()
+        {
+            return String.Format("Name: {0}, Email: {1}, Age: {2}", name, email, age);
+        }
+    }
+}
